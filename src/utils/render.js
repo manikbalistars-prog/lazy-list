4fb8f868bce614332fdfa43c43206a30c4d77374
manikbalistars-prog <manik.balistars@gmail.com>
2026-06-14 09:35:50 +0800

    add color for offday

--- src/utils/render.js
+++ src/utils/render.js
@@ -13,10 +13,9 @@ import { loadUser } from "../services/users";
 const calendarEl = document.getElementById("calendarEl");
 const userListEl = document.getElementById("userList");
 
-let calendarInstance = null; 
+let calendarInstance = null;
 
 export async function initCalendar() {
-
   if (calendarInstance) return calendarInstance;
 
   const valid = await isValidUser();
@@ -43,16 +42,23 @@ export async function initCalendar() {
     eventContent: function (arg) {
       const isHalf = arg.event.extendedProps.isHalf;
       const name = arg.event.title;
+      let bgClass;
+
+      if (isHalf) {
+        bgClass = "bg-yellow-600";
+      } else if (name === "DAY OFF") {
+        bgClass = "bg-red-600";
+      } else {
+        bgClass = "bg-blue-600";
+      }
 
       return {
         html: `
-          <div class="flex justify-between items-center px-1.5 rounded-sm text-white w-full ${
-            isHalf ? "bg-yellow-600" : "bg-blue-600"
-          } ">
-            ${name}
-            ${isHalf ? '<span class="text-xs opacity-70">½</span>' : ""}
-          </div>
-        `,
+  <div class="flex justify-between items-center px-1.5 rounded-sm text-white w-full ${bgClass}">
+    ${name}
+    ${isHalf ? '<span class="text-xs opacity-70">½</span>' : ""}
+  </div>
+`,
       };
     },
 
@@ -138,7 +144,6 @@ export async function initCalendar() {
 
   calendarInstance.render();
 
-
   subscribeOffDay((raw) => {
     if (!calendarInstance) return;
 
@@ -168,7 +173,7 @@ export async function renderUsers() {
   let users = [];
 
   if (admin) {
-    users = await loadUser(); 
+    users = await loadUser();
   } else {
     const raw = localStorage.getItem("user");
     if (!raw) return;
@@ -180,7 +185,7 @@ export async function renderUsers() {
         id: user.id,
         name: user.name,
       },
-    ]; 
+    ];
   }
 
   userListEl.innerHTML = users
